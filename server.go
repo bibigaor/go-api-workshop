bf11a082ea0ec6b21c4d1a183bf1267d893ca9dd
bibigaor <bibipsika@gmail.com>
2017-05-29 02:30:55 -0500

    Add files via upload

--- server.go
+++ server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"net/url"
 	"net/http"
+	"log"
 )
 
 type User struct {
@@ -18,104 +20,52 @@ type WorkerConf struct {
 var (
 	userChan   chan User
 	resultChan chan string
+	//url chan string
 )
 
 func main() {
 	// Initialize channels
+	id := "1810"
 	userChan = make(chan User)
 	resultChan = make(chan string)
+	idwork := url.QueryEscape(id)
+	url := fmt.Sprintf("http://localhost.3001/workers/validate?idworker=%s",idwork)
 
-	// Run an instance though a goroutine
-	conf1 := WorkerConf{":3001"}
-	go mainApiWorker(conf1)
+	
+	go mainApiWorker()
 
-	go handleUser("Handler 1")
-	go handleUser("Handler 2")
 
-	// Run an instance in the main thread
-	conf2 := WorkerConf{":3002"}
-	altApiWorker(conf2)
+
 }
 
-func mainApiWorker(conf WorkerConf) {
-	router := gin.Default()
+func mainApiWorker() {
 
-	v1 := router.Group("/v1")
-	{
-		v1.GET("/hello/:name", getHello)
-		v1.GET("/hello", getHello)
-		v1.PUT("/user/:id", putUser)
+	//se realiza el request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
 	}
-	router.Run(conf.Port) // listen and server on 0.0.0.0:3001
-}
 
-func altApiWorker(conf WorkerConf) {
-	router := gin.Default()
+	client := &http.Client{}
 
-	admin := router.Group("/admin")
-	{
-		admin.GET("/hello", getHelloAdmin)
-		admin.POST("/user", postUser)
+// Se valida que el request sea exitoso
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
 	}
-	router.Run(conf.Port) // listen and server on 0.0.0.0:3002
-}
+	defer resp.Body.Close()
 
-func putUser(c *gin.Context) {
-	id := c.Param("id")
-	other := c.DefaultQuery("page", "0") // shortcut for c.Request.URL.Query().Get("page")
-	message := c.DefaultPostForm("message", "Nothing over here")
+	var record User
 
-	str := fmt.Sprintf("Id: %s, Message: %s, Other: %s", id, message, other)
-	c.JSON(http.StatusOK, gin.H{
-		"message": str,
-	})
-}
 
-func postUser(c *gin.Context) {
-	var user User
-
-	if c.BindJSON(&user) == nil {
-		userChan <- user
-		result := <-resultChan
-		if result == "OK" {
-			c.JSON(http.StatusOK, gin.H{"status": "you are italian"})
-			return
-		} else {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-			return
-		}
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		log.Println(err)
 	}
-}
+	//se obtiene el usuario y contraseña
+	 Nombre := record.Name
+	 Password := record.Password
 
-func getHello(c *gin.Context) {
-	var str string
-	name := c.Param("name")
-	if name != "" {
-		str = fmt.Sprint("Hello, ", name)
-	} else {
-		str = fmt.Sprint("Hello, World!")
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"message": str,
-	})
-}
 
-func getHelloAdmin(c *gin.Context) {
-	var str string
-	str = fmt.Sprint("Hello, Admin!")
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": str,
-	})
-}
-
-func handleUser(workerName string) {
-	for user := range userChan {
-		fmt.Printf("[%s] Got request for user %s\n", workerName, user.Name)
-		if user.Name == "sergio" && user.Password == "pizza" {
-			resultChan <- "OK"
-		} else {
-			resultChan <- "Darn"
-		}
-	}
 }
